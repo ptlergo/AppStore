9166f146821b331984c84f3e81e0e524e0d53c3c
ptlergo <ptlergo@gmail.com>
2016-08-02 16:47:07 -0400

    bodyparser implemented.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 
-const port = 3000;
+//using body-parser grab data from POST
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json({hello: 'world'});
