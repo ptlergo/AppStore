8653d2a5b8994b4aede423fa2787d3b04d17ea9f
ptlergo <ptlergo@gmail.com>
2016-08-02 16:00:07 -0400

    server built and running with base route

--- src/server.js
+++ src/server.js
@@ -1,2 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({hello: 'world'});
+})
+
+app.listen(port, () => {
+  console.log('server active on ', port);
+});
