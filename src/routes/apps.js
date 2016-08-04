68505401b0de6c8f873f9ed2d6114a4e2ce8884e
ptlergo <ptlergo@gmail.com>
2016-08-04 13:18:28 -0400

    creating apps.js for apps routes

---
+++ src/routes/apps.js
@@ -0,0 +1,31 @@
+module.exports = (express) => {
+  const router = express.Router();
+
+  // GET all apps
+  router.get('/apps', (req, res) => {
+    res.json([
+      {
+        id: 1,
+        title: 'app1',
+        description: 'first app',
+        releaseDate: '2016-08-04T16:52:49+00:00',
+      },
+      {
+        id: 2,
+        title: 'app2',
+        description: '2nd app',
+        releaseDate: '2016-08-04T16:52:49+00:00',
+      },
+    ]);
+  });
+
+  // GET app of 'id'
+  router.get('/apps/:id', (req, res) => {
+    res.json({
+      id: 1,
+      title: 'app1',
+      description: 'first app',
+      releaseDate: '2016-08-04T16:52:49+00:00',
+    });
+  });
+};
