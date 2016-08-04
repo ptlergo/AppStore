d59107445fbb64052945a5c5671ab2ee53dd605c
ptlergo <ptlergo@gmail.com>
2016-08-04 03:45:24 -0400

    unit tests apps success. apps routes built with static objects

--- src/routes/api.js
+++ src/routes/api.js
@@ -11,13 +11,26 @@ module.exports = (express) => {
       {
         id: 1,
         title: 'app1',
+        description: 'first app',
+        releaseDate: '1/1/1991',
       },
-      { id: 2 },
+      {
+        id: 2,
+        title: 'app2',
+        description: '2nd app',
+        releaseDate: '1/1/1991',
+      },
     ]);
   });
 
   router.get('/v1/apps/:id', (req, res) => {
-    res.json({ id: req.params.id });
+    const appId = req.params.id;
+    res.json({
+      id: appId,
+      title: 'app1',
+      description: 'first app',
+      releaseDate: '1/1/1991',
+    });
   });
 
   return router;
