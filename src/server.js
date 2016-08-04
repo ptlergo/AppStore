b6b346d779ef7c13b7b5dc377e1c71fe0dd69c39
ptlergo <ptlergo@gmail.com>
2016-08-04 12:45:20 -0400

    version control added to router registration in server.js

--- src/server.js
+++ src/server.js
@@ -10,10 +10,8 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 // Router file registration with ''/api' prefix. pass in express to file.
-app.use('/api', require('./routes/api')(express));
+app.use('/api/v1', require('./routes/api')(express));
 
-const server = app.listen(port, () => {
+exports.server = app.listen(port, () => {
   console.log('server active on ', port);
 });
-
-module.exports = server;
