7f5e921ceba0fcb5f3463b268efe0d7d05c776a7
ptlergo <ptlergo@gmail.com>
2016-08-05 00:21:59 -0400

    apps route touch up

--- src/routes/apps.js
+++ src/routes/apps.js
@@ -1,11 +1,6 @@
 module.exports = (express) => {
   const router = express.Router();
 
-  // Routes apps
-  router.get('/', (req, res) => {
-    res.json({ work: 'it works works works works works' });
-  });
-
   // GET all apps
   router.get('/apps', (req, res) => {
     res.json([
