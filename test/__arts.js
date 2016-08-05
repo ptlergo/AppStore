a25c73961aebb56054ef0006b51d4320c87b3615
ptlergo <ptlergo@gmail.com>
2016-08-05 00:30:10 -0400

    mocha arts stubs created. PASS

--- test/__arts.js
+++ test/__arts.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 
-describe('art route testing ', () => {
+describe('Arts Routes', () => {
   let server;
 
   beforeEach(() => {
@@ -12,7 +12,19 @@ describe('art route testing ', () => {
   afterEach(() => {
     // Close server after each stub
     server.close();
-  })
+  });
+
+  it('should be an art asset', (done) => {
+    request(server)
+    .get('/api/v1/arts')
+    .set('Accept', 'application/json')
+    .expect('Content-Type', /json/)
+    .expect(() => {
+      expect(1).to.be.above(0);
+    })
+    .end(done);
+  });
+
   it('should be an art asset', (done) => {
     done();
   });
