33e374fb26ca42b0f8290a4e272e3264ea93fb17
ptlergo <ptlergo@gmail.com>
2016-08-04 23:29:17 -0400

    mocha tests fail

---
+++ src/routes/api.js
@@ -0,0 +1,14 @@
+module.exports = (express) => {
+  const router = express.Router();
+
+  // Routes apps
+  router.get('/', (req, res) => {
+    res.json({ hello: 'world' });
+  });
+
+  // individual routes
+  router.use('/api/v1/', require('./apps')(express));
+  router.use('/api/v1/', require('./users')(express));
+
+  return router;
+};
