2f9d62e45c0bde580dfbfe36918b05a3cce12e71
ptlergo <ptlergo@gmail.com>
2016-08-04 20:28:55 -0400

    users route file created

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,27 +10,5 @@ module.exports = (express) => {
   router.use('/api/v1/', require('./apps')(express));
   router.use('/api/v1/', require('./users')(express));
 
-  // GET all users
-  router.get('/users', (req, res) => {
-    res.json([
-      {
-        id: 1,
-        name: 'user1',
-      },
-      {
-        id: 2,
-        name: 'user2',
-      },
-    ]);
-  });
-
-  // GET user of id
-  router.get('/users/:id', (req, res) => {
-    res.json({
-      id: 1,
-      name: 'user1',
-    });
-  });
-
   return router;
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,28 +1,27 @@
 module.exports = (express) => {
-  
 const router = express.Router();
 
 // GET all users
-router.get('/users', (req, res) => {
-  res.json([
-    {
+  router.get('/users', (req, res) => {
+    res.json([
+      {
+        id: 1,
+        name: 'user1',
+      },
+      {
+        id: 2,
+        name: 'user2',
+      },
+    ]);
+  });
+
+  // GET user of id
+  router.get('/users/:id', (req, res) => {
+    res.json({
       id: 1,
       name: 'user1',
-    },
-    {
-      id: 2,
-      name: 'user2',
-    },
-  ]);
-});
-
-// GET user of id
-router.get('/users/:id', (req, res) => {
-  res.json({
-    id: 1,
-    name: 'user1',
+    });
   });
-});
 
   return router;
 };
