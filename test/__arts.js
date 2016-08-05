f08716b3e4b13ddbdf870c4446e57e785ca27114
ptlergo <ptlergo@gmail.com>
2016-08-05 00:06:27 -0400

    unit test for art asset route not yet constructed

---
+++ test/__arts.js
@@ -0,0 +1,8 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+
+describe('art route testing ', () => {
+  it('should be an art asset', (done) => {
+    done();
+  });
+});
