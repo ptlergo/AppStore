e0ffa61a380bc7df215670d51af0bd16e16c2089
ptlergo <ptlergo@gmail.com>
2016-08-07 19:51:37 -0400

    dotenv package save

--- src/models/db.js
+++ src/models/db.js
@@ -1,9 +1,10 @@
 const Sequelize = require('sequelize');
 
+require('dotenv').config();
 // Call constructor function to create new object
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: 'localhost',
-  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_SCHEMA,
 
   pool: {
     max: 5,
