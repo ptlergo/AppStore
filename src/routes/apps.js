e470aaf4087f9a521686541b76d4449bfe438e7d
ptlergo <ptlergo@gmail.com>
2016-08-07 01:00:46 -0400

    constructing create for apps and get for apps. functional but incomplete

--- src/routes/apps.js
+++ src/routes/apps.js
@@ -1,22 +1,24 @@
 module.exports = (express) => {
   const router = express.Router();
 
+  // CREATE new app
+  router.route('/apps')
+    .post((req) => {
+      const obj = req.body;
+      const appnew = {
+        id: obj.id,
+        title: obj.title,
+        description: obj.description,
+        releaseDate: obj.releaseDate,
+      };
+      // debug log
+      console.log(appnew);
+    });
+
   // GET all apps
   router.get('/apps', (req, res) => {
-    res.json([
-      {
-        id: 1,
-        title: 'app1',
-        description: 'first app',
-        releaseDate: '2016-08-04T16:52:49+00:00',
-      },
-      {
-        id: 2,
-        title: 'app2',
-        description: '2nd app',
-        releaseDate: '2016-08-04T16:52:49+00:00',
-      },
-    ]);
+    const appstuff = req.body;
+    res.json(appstuff);
   });
 
   // GET app of 'id'
@@ -28,12 +30,6 @@ module.exports = (express) => {
       releaseDate: '2016-08-04T16:52:49+00:00',
     });
   });
-
-  // POST app
-  router.route('/apps/:id')
-    .post((req, res) => {
-      const appId = req.params.id;
-    });
 
   return router;
 };
