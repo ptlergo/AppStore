59c284144243bc46705944db033a7fff4c97dd73
ptlergo <ptlergo@gmail.com>
2016-08-07 20:34:39 -0400

    find one app model detail added

--- src/models/app.js
+++ src/models/app.js
@@ -1,6 +1,28 @@
 const db = require('./db');
 
+// CREATE into database
 exports.create = (payload, err, success) => {
   // Use sequelize method create
   db.app.create(payload).then(success).catch(err);
+};
+
+// READ all from database
+exports.findAll = (err, success) => {
+  // Use sequelize method findAll
+  db.app.findAll().then(success).catch(err);
+};
+
+// READ single from database
+exports.find = (payload, err, success) => {
+  // Use sequelize method find
+  db.app.find({
+    where: {
+      id: payload.id,
+    },
+    // Find all relations from sequelize
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(success).catch(err);
 };
