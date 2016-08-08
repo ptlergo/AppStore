c654074f3b080e8996dfeb9b35794b688da9b261
ptlergo <ptlergo@gmail.com>
2016-08-07 22:45:54 -0400

    user model added

---
+++ src/models/user.js
@@ -0,0 +1,52 @@
+const db = require('./db');
+
+// READ(all) all from database
+exports.all = (errorCallback, successCallBack) => {
+  // Use sequelize method findAll
+  db.user.findAll().then(successCallBack).catch(errorCallback);
+};// END of all
+
+// CREATE(add) into database
+exports.add = (obj, errorCallbackorCallback, successCallBack) => {
+  // Use sequelize method create
+  db.user.create(obj).then(successCallBack).catch(errorCallbackorCallback);
+};// END of add
+
+// READ(one) single from database
+exports.one = (id, errorCallback, successCallBack) => {
+  // Use sequelize method find
+  db.user.find({
+    where: {
+      id: id.id,
+    },
+    // Find all relations from sequelize
+    include: [{
+      all: true,
+      nested: true,
+    }],
+  }).then(successCallBack).catch(errorCallback);
+};// END of one
+
+// UPDATE single from database
+exports.update = (obj, errorCallback, successCallBack) => {
+  // Use sequelize method find
+  db.user.find({
+    where: {
+      id: obj.id,
+    },
+  })
+  // Promise to take current data and update it with new user data
+  .then((existingData) => {
+    existingData.updateAttributes(obj).then(successCallBack).catch(errorCallback);
+  }).catch(errorCallback);
+};// END of update
+
+// DELETE(remove) single from database
+exports.remove = (id, errorCallback, successCallBack) => {
+  // Use sequelize method find
+  db.user.destroy({
+    where: {
+      id: id.id,
+    },
+  }).then(successCallBack).catch(errorCallback);
+};// END of remove
