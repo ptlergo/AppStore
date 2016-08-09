9dfe7c9b5dab90fa9665e4d48c660d111f069bdf
ptlergo <ptlergo@gmail.com>
2016-08-09 01:35:07 -0400

    removed faker from unit test app route. tests work except a few

--- test/__routes_apps.js
+++ test/__routes_apps.js
@@ -1,50 +1,20 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
 const request = require('supertest');
-const faker = require('faker');
-const App = require('../src/models/app');
 
 // Unit Test for APP Routes
 describe('App Routes', () => {
   let server;
   let app;
-  let testApp = {};
 
   // Open server at each stub
-  beforeEach((done) => {
+  beforeEach(() => {
     server = require('../src/server');
-    // Create an app with temp. details
-    const fakeApp = {
-      id: faker.random.number(),
-      title: faker.name.title(),
-      description: faker.company.catchPhraseDescriptor(),
-      releaseDate: faker.phone.phoneNumber(),
-    };
-    // Add fake app data into database
-    App.add(fakeApp,
-      (err) => {
-        throw new Error(err);
-      },
-      (data) => {
-        testApp = data;
-        done();
-      }
-    );
   });
 
   // Close server after each stub done
-  afterEach((done) => {
+  afterEach(() => {
     server.close();
-
-    App.remove(testApp,
-      (err) => {
-        throw new Error(err);
-      },
-      (data) => {
-        expect(data).to.be.equal(1);
-        done();
-      }
-    );
   });
 
   // Test route of all apps
@@ -57,7 +27,7 @@ describe('App Routes', () => {
         const apps = res.body;
 
         // Save one single app from the list to test on in later tests
-        this.app = testApp;
+        this.app = apps[0];
         expect(apps.length).to.be.above(0);
       })
       .end(done);
