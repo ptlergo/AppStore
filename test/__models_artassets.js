64e1e194b45f2d5caf9845389316263e0167c2b2
ptlergo <ptlergo@gmail.com>
2016-08-08 21:11:08 -0400

    art asset model unit tests stub skeletons built

--- test/__models_artassets.js
+++ test/__models_artassets.js
@@ -2,7 +2,23 @@ const expect = require('chai').expect;
 const assert = require('chai').assert;
 
 describe('Art Assets Model', () => {
-  it('should return true', (done) => {
+  it('should GET all art assets', (done) => {
+    done();
+  });
+
+  it('should Add an art assets', (done) => {
+    done();
+  });
+
+  it('should GET a single art asset', (done) => {
+    done();
+  });
+
+  it('should UPDATE an art asset', (done) => {
+    done();
+  });
+
+  it('should DELETE an art asset', (done) => {
     done();
   });
 });// END of descirbe
