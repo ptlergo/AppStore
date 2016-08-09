274c1c6923263215e99e732b171cb4cd888150d4
ptlergo <ptlergo@gmail.com>
2016-08-09 02:03:02 -0400

    art asset update route built

--- src/routes/api/arts.js
+++ src/routes/api/arts.js
@@ -32,10 +32,10 @@ module.exports = (express) => {
 
   // Route READ a single art asset
   router.get('/artassets/:id', (req, res) => {
-    const artId = req.body;
-    artId.id = req.params.id;
+    const artInfo = req.body;
+    artInfo.id = req.params.id;
     // Find one art asset
-    ArtAsset.one(
+    ArtAsset.one(artInfo,
       (err) => {
         res.status(500).json(err);
       },
@@ -47,7 +47,17 @@ module.exports = (express) => {
 
   // Route UPDATE single art asset
   router.post('/artassets/:id', (req, res) => {
-
+    const artInfo = req.body;
+    artInfo.id = req.params.id;
+    // Find one art asset to update
+    ArtAsset.update(artInfo,
+      (err) => {
+        res.status(500).json(err);
+      },
+      (data) => {
+        res.status(200).json(data);
+      }
+    );
   });
 
   // Route DELETE single art asset
