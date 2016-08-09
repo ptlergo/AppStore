cfb07f0c1dad9497e5b2f1dd387064527911706a
ptlergo <ptlergo@gmail.com>
2016-08-08 21:41:09 -0400

    building art asset model. stub get all art assets

--- test/__models_artassets.js
+++ test/__models_artassets.js
@@ -1,9 +1,22 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
+const faker = require('faker');
+const ArtAsset = require('../src/models/artasset');
 
 describe('Art Assets Model', () => {
+  let testArtAsset;
+
+  // Test for all art assets
   it('should GET all art assets', (done) => {
-    done();
+    ArtAsset.all(
+      (err) => {
+        throw new Error(err);
+      },
+      (artassets) => {
+        this.testArtAsset = artassets;
+        done();
+      }
+    );
   });
 
   it('should Add an art assets', (done) => {
