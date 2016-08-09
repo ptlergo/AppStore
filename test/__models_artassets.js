833762a867e3730c724ca68d366431c992ca096a
ptlergo <ptlergo@gmail.com>
2016-08-08 21:09:03 -0400

    mocha test building art assets model

--- test/__models_artassets.js
+++ test/__models_artassets.js
@@ -1,2 +1,8 @@
 const expect = require('chai').expect;
 const assert = require('chai').assert;
+
+describe('Art Assets Model', () => {
+  it('should return true', (done) => {
+    done();
+  });
+});// END of descirbe
