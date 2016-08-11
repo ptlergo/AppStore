c12a792108d7b0e73a8626f6c2e51adaeb88f0ec
ptlergo <ptlergo@gmail.com>
2016-08-11 01:30:42 -0400

    paramter unit test stub pass for util

--- test/__util.js
+++ test/__util.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const util = require('../src/lib/util');
+const util = require('../src/lib/util').debug;
 
 describe('Util Tool', () => {
   it('should successfully use DEBUG mode', (done) => {
@@ -7,10 +7,13 @@ describe('Util Tool', () => {
   });
 
   it('should successfully grab 2 parameters in debug method', (done) => {
+    const testObj = 'testing util string parameter';
+    const testObj2 = 3;
+    expect(util(testObj, testObj2)).to.not.be.arguments;
     done();
   });
 
   it('should successfully output to logs folder', (done) => {
     done();
   });
-});// END of App Model describe
+});// END of Util Tool describe
