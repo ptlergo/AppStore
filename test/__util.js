d071c189df4418e350b2dde1c2afe20b8f5eaa44
ptlergo <ptlergo@gmail.com>
2016-08-10 23:27:16 -0400

    unit test for util successful

--- test/__util.js
+++ test/__util.js
@@ -3,6 +3,8 @@ const Util = require('../src/lib/util');
 
 describe('Util Tool', () => {
   it('should output to console from debug method', (done) => {
+    console.log(Util.debug('hh'));
+    expect(Util.debug.length).to.be.above(0);
     done();
   });
 });// END of App Model describe
