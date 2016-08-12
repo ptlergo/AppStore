178c3c31be2c92b51496f0dcfc84b7ff8d1c68e0
ptlergo <ptlergo@gmail.com>
2016-08-11 22:31:28 -0400

    file write stream created for util

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,6 +1,10 @@
-exports.debug = (obj) => {
-  console.log(obj.msg + obj.term);
+const fs = require('fs');
+// Create file to write to
+const writableStream = fs.createWriteStream('./logs/log.log', { flag: 'a' });
 
+exports.debug = (obj) => {
+  // Write to log file
+  writableStream.write(obj.msg);
   return {
     msg: obj.msg,
     term: obj.term,
