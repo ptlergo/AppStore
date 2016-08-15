6c9b3fd0e07b7254e4d71bffe16b03499b1ea539
ptlergo <ptlergo@gmail.com>
2016-08-14 23:30:51 -0400

    route array filled for user

--- test/__dynamic_routes.js
+++ test/__dynamic_routes.js
@@ -1,14 +1,33 @@
 const expect = require('chai').expect;
+const request = require('supertest');
 
 // Array of all routes in API
 const routesArray = [
+  // User Routes
   {
     route: '/api/v1/users',
     method: 'get',
+    desc: 'read all users',
   },
   {
     route: '/api/v1/users/:id',
     method: 'get',
+    desc: 'read one user',
+  },
+  {
+    route: '/api/v1/users/:id',
+    method: 'delete',
+    desc: 'delete one user',
+  },
+  {
+    route: '/api/v1/users/:id',
+    method: 'post',
+    desc: 'update one user',
+  },
+  {
+    route: '/api/v1/users/',
+    method: 'post',
+    desc: 'create one user',
   },
 ];
 
@@ -16,8 +35,10 @@ describe('TESTING ROUTES DYNAMICALLY ', () => {
   let server;
 
   // Start server before stubs
-  beforeEach('open server', () => {
+  beforeEach('open server', (done) => {
     server = require('../src/server');
+
+    done();
   });
 
   // Close server after stubs
