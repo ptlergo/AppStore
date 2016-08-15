0c56acda5f10a06338c12f9132a44c73233f49a5
ptlergo <ptlergo@gmail.com>
2016-08-15 01:28:28 -0400

    routes object constructed successfully

--- test/__dynamic_routes.js
+++ test/__dynamic_routes.js
@@ -18,43 +18,8 @@ describe('TESTING ROUTES DYNAMICALLY ', () => {
   });
 
   // Array of all routes in API
-  const routesArray = [
-    // User Routes
-    {
-      route: '/api/v1/users',
-      method: 'get',
-      desc: 'read all users',
-    },
-    {
-      route: '/api/v1/users/:id',
-      method: 'get',
-      desc: 'read one user',
-    },
-    // {
-    //   route: '/api/v1/users/:id',
-    //   method: 'delete',
-    //   desc: 'delete one user',
-    // },
-    // {
-    //   route: '/api/v1/users/:id',
-    //   method: 'post',
-    //   desc: 'update one user',
-    //   fakeData: {
-    //     name: faker.name.findName(),
-    //     age: faker.random.number(),
-    //   },
-    // },
-    // {
-    //   route: '/api/v1/users/',
-    //   method: 'post',
-    //   desc: 'create one user',
-    //   fakeData: {
-    //     name: faker.name.findName(),
-    //     age: faker.random.number(),
-    //   },
-    // },
-  ];
-  const obj = {
+  const routesObj = {
+    // User routes
     users: [
       {
         route: '/api/v1/users',
@@ -66,7 +31,31 @@ describe('TESTING ROUTES DYNAMICALLY ', () => {
         method: 'get',
         desc: 'read one user',
       },
+      {
+        route: '/api/v1/users/:id',
+        method: 'delete',
+        desc: 'delete one user',
+      },
+      {
+        route: '/api/v1/users/:id',
+        method: 'post',
+        desc: 'update one user',
+        fakeData: {
+          name: faker.name.findName(),
+          age: faker.random.number(),
+        },
+      },
+      {
+        route: '/api/v1/users/',
+        method: 'post',
+        desc: 'create one user',
+        fakeData: {
+          name: faker.name.findName(),
+          age: faker.random.number(),
+        },
+      },
     ],
+    // App routes
     apps: [
       {
         route: '/api/v1/apps',
@@ -78,23 +67,91 @@ describe('TESTING ROUTES DYNAMICALLY ', () => {
         method: 'get',
         desc: 'read one app',
       },
-    ] };
+      {
+        route: '/api/v1/users/:id/apps',
+        method: 'get',
+        desc: 'read all apps of one user',
+      },
+      {
+        route: '/api/v1/apps/:id',
+        method: 'delete',
+        desc: 'delete one app',
+      },
+      {
+        route: '/api/v1/apps/:id',
+        method: 'post',
+        desc: 'update one app',
+        fakeData: {
+          title: faker.name.title(),
+          description: faker.company.catchPhraseDescriptor(),
+          releaseDate: faker.phone.phoneNumber(),
+        },
+      },
+      {
+        route: '/api/v1/apps/',
+        method: 'post',
+        desc: 'create one app',
+        fakeData: {
+          title: faker.name.title(),
+          description: faker.company.catchPhraseDescriptor(),
+          releaseDate: faker.phone.phoneNumber(),
+        },
+      },
+    ],
+    // Artasset routes
+    artassets: [
+      {
+        route: '/api/v1/users',
+        method: 'get',
+        desc: 'read all users',
+      },
+      {
+        route: '/api/v1/users/:id',
+        method: 'get',
+        desc: 'read one user',
+      },
+      {
+        route: '/api/v1/users/:id',
+        method: 'delete',
+        desc: 'delete one user',
+      },
+      {
+        route: '/api/v1/users/:id',
+        method: 'post',
+        desc: 'update one user',
+        fakeData: {
+          name: faker.name.findName(),
+          age: faker.random.number(),
+        },
+      },
+      {
+        route: '/api/v1/users/',
+        method: 'post',
+        desc: 'create one user',
+        fakeData: {
+          name: faker.name.findName(),
+          age: faker.random.number(),
+        },
+      },
+    ],
+  }; // END of routes object
+
   // Functions to test methods
   function getMethod(x) {
     let s = console.log(x.users[0].route);
-    if (routesArray.method === 'get') {
-      s = console.log('working', routesArray.method);
+    if (routesObj.method === 'get') {
+      s = console.log('working', routesObj.method);
     }
     return s;
   }
 
-  Object.keys(obj).forEach((prop) => {
+  Object.keys(routesObj).forEach((prop) => {
     console.log(prop);
   });
 
 
   it('should be working', (done) => {
-    getMethod(obj);
+    getMethod(routesObj);
     done();
   });
 });
