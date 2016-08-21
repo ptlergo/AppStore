a4e73ac67a8a6275b5a10f1d2810d476298e8f63
ptlergo <ptlergo@gmail.com>
2016-08-20 20:24:41 -0400

    unit test for debug function stub built

--- test/__util.js
+++ test/__util.js
@@ -5,17 +5,29 @@ const sinon = require('sinon');
 const spy = sinon.spy();
 const util = require('../src/lib/util');
 
-const statement = util.debug({ msg: 'testing', info: 'parameter' });
+// Test object with utility tools acceptable parameters
+const testObj = {
+  msg: 'testing',
+  info: 'parameter',
+};
 
-describe('Util Tool debug()', () => {
-  it('should successfully call callAPI', (done) => {
-    spy(util, 'callAPI');
+// Test utility tool
+describe('Util Tool', () => {
+  it('should have a function debug()', (done) => {
+    expect(util.debug).to.be.a('function');
+    done();
+  });
+
+  it('should successfully call debug', (done) => {
+    console.log(spy(util, 'debug'));
+    util.debug(testObj);
+    expect(util.debug.calledOnce).to.be.true;
     done();
   });
 
   it('should successfully grab an object as the only parameter', (done) => {
-    expect(statement).to.have.ownProperty('msg');
-    expect(statement).to.have.ownProperty('info');
+    expect(util.debug(testObj)).to.have.ownProperty('msg');
+    expect(util.debug(testObj)).to.have.ownProperty('info');
     done();
   });
 
