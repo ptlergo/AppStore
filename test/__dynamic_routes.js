e0a7a38f9a1b0263ad0a3696dd42db057ea90486
ptlergo <ptlergo@gmail.com>
2016-08-22 13:18:26 -0400

    removed redundant dependencies

--- test/__dynamic_routes.js
+++ test/__dynamic_routes.js
@@ -1,7 +1,6 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 const faker = require('faker');
-const async = require('async');
 const util = require('../src/lib/util').debug;
 
 // Test API Routes
