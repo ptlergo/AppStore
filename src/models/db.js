668dfad63d335937d5abf95a194be4273eb347fe
ptlergo <ptlergo@gmail.com>
2016-08-22 01:34:50 -0400

    db file update

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-require('localenvironment');
+if (!process.env.DB_HOST) require('localenvironment');
 // Call constructor function to create new object
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
