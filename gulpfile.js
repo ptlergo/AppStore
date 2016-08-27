56985e4af681b4902fda2d3125052d5278a3a2c8
ptlergo <ptlergo@gmail.com>
2016-08-27 15:18:18 -0400

    gulp task mocha update

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
 gulp.task('default', () => {
-  return gulp.src(['test/*.js'], { read: false })
+  gulp.src(['test/*.js'], { read: false })
     .pipe(mocha({
       reporter: 'spec',
     }));
